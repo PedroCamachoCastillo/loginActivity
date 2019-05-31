542bd0a3c5194bda859d7722cf5d9ca99b64e5d8
PedroCamachoCastillo <163168@ids.upchiapas.edu.mx>
2019-05-31 11:23:14 -0500

    456160

--- CS8/settings.py
+++ CS8/settings.py
@@ -159,7 +159,7 @@ STATIC_URL = '/static/'
 # Update database configuration with $DATABASE_URL.
 #import dj_database_url  
 #db_from_env = dj_database_url.config(conn_max_age=500)  
-#DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 
